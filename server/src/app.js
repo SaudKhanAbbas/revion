a7e31980e77c169a08647bf06a8293b9d36e83f0
SaudKhanAbbas <saudkhanabbas@gmail.com>
2026-07-07 11:13:59 +0530

    Create user model and authentication routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,13 @@
 import express from "express";
 import cors from "cors";
 
+import authRoutes from "./routes/auth.routes.js";
+
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Health Check Route
 app.get("/api/health", (req, res) => {
   res.status(200).json({
     status: "OK",
@@ -15,5 +15,7 @@ app.get("/api/health", (req, res) => {
     timestamp: new Date().toISOString(),
   });
 });
+
+app.use("/api/auth", authRoutes);
 
 export default app;
---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const register = async (req, res) => {
+  res.json({
+    message: "Register endpoint working",
+  });
+};
+
+export const login = async (req, res) => {
+  res.json({
+    message: "Login endpoint working",
+  });
+};
